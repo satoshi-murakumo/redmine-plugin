f96631981dd83c6aedcdaad549f5682ad87e4b24
shida <shida@lac.co.jp>
2012-06-29 15:52:55 +0900

    convert tabs to spaces. delete trailing spaces.

--- src/main/java/hudson/plugins/redmine/RedmineRepositoryBrowser.java
+++ src/main/java/hudson/plugins/redmine/RedmineRepositoryBrowser.java
@@ -17,96 +17,96 @@ import hudson.scm.SubversionChangeLogSet.Path;
 
 /**
  * produces redmine links.
- * 
+ *
  * @author gaooh
  * @date 2008/10/26
  */
 public class RedmineRepositoryBrowser extends SubversionRepositoryBrowser {
 
-	@DataBoundConstructor
+    @DataBoundConstructor
     public RedmineRepositoryBrowser() {
     }
-	
-	@Override
-	public URL getDiffLink(Path path) throws IOException {
-		if(path.getEditType()!= EditType.EDIT) {
-            return null;    
-		}
+
+    @Override
+    public URL getDiffLink(Path path) throws IOException {
+        if(path.getEditType()!= EditType.EDIT) {
+            return null;
+        }
         URL baseUrl = getRedmineURL(path.getLogEntry());
         String projectName = getProject(path.getLogEntry());
         String filePath = getFilePath(path.getLogEntry(), path.getValue());
-        
+
         int revision = path.getLogEntry().getRevision();
         return new URL(baseUrl, "repositories/diff/" + projectName + filePath + "?rev=" + revision);
-	}
+    }
 
-	@Override
-	public URL getFileLink(Path path) throws IOException {
-		URL baseUrl = getRedmineURL(path.getLogEntry());
-		String projectName = getProject(path.getLogEntry());
-		String filePath = getFilePath(path.getLogEntry(), path.getValue());
-        
+    @Override
+    public URL getFileLink(Path path) throws IOException {
+        URL baseUrl = getRedmineURL(path.getLogEntry());
+        String projectName = getProject(path.getLogEntry());
+        String filePath = getFilePath(path.getLogEntry(), path.getValue());
+
         return baseUrl == null ? null : new URL(baseUrl, "repositories/entry/" + projectName + filePath);
-	}
+    }
 
-	@Override
-	public URL getChangeSetLink(LogEntry changeSet) throws IOException {
-		URL baseUrl = getRedmineURL(changeSet);
-		String projectName = getProject(changeSet);
+    @Override
+    public URL getChangeSetLink(LogEntry changeSet) throws IOException {
+        URL baseUrl = getRedmineURL(changeSet);
+        String projectName = getProject(changeSet);
         return baseUrl == null ? null : new URL(baseUrl, "repositories/revision/" + projectName + "/" + changeSet.getRevision());
-	}
+    }
 
-	@Override
-	public Descriptor<RepositoryBrowser<?>> getDescriptor() {
-		 return DESCRIPTOR;
-	}
-	
-	private URL getRedmineURL(LogEntry logEntry) throws MalformedURLException {
+    @Override
+    public Descriptor<RepositoryBrowser<?>> getDescriptor() {
+         return DESCRIPTOR;
+    }
+
+    private URL getRedmineURL(LogEntry logEntry) throws MalformedURLException {
         AbstractProject<?,?> p = (AbstractProject<?,?>)logEntry.getParent().build.getProject();
         RedmineProjectProperty rpp = p.getProperty(RedmineProjectProperty.class);
         if(rpp == null) {
-        	return null;
+            return null;
         } else {
-        	return new URL(rpp.redmineWebsite);
+            return new URL(rpp.redmineWebsite);
         }
     }
 
-	private String getProject(LogEntry logEntry) {
-		AbstractProject<?,?> p = (AbstractProject<?,?>)logEntry.getParent().build.getProject();
-		RedmineProjectProperty rpp = p.getProperty(RedmineProjectProperty.class);
+    private String getProject(LogEntry logEntry) {
+        AbstractProject<?,?> p = (AbstractProject<?,?>)logEntry.getParent().build.getProject();
+        RedmineProjectProperty rpp = p.getProperty(RedmineProjectProperty.class);
         if(rpp == null) {
-        	return null;
+            return null;
         } else {
-        	return rpp.projectName;
+            return rpp.projectName;
         }
-	}
-	
-	private String getFilePath(LogEntry logEntry, String fileFullPath) {
-		AbstractProject<?,?> p = (AbstractProject<?,?>)logEntry.getParent().build.getProject();
-		RedmineProjectProperty rpp = p.getProperty(RedmineProjectProperty.class);
-		
-		String filePath = "";
+    }
+
+    private String getFilePath(LogEntry logEntry, String fileFullPath) {
+        AbstractProject<?,?> p = (AbstractProject<?,?>)logEntry.getParent().build.getProject();
+        RedmineProjectProperty rpp = p.getProperty(RedmineProjectProperty.class);
+
+        String filePath = "";
         if(rpp.redmineVersion.booleanValue()) { // 0.8.1 or after
-        	filePath = fileFullPath;
-        	
+            filePath = fileFullPath;
+
         } else { // 0.8.0 or before
-        	String[] filePaths = fileFullPath.split("/");
-        	filePath = "/";
-        	if(filePaths.length > 2) {
-        		for(int i = 2 ; i < filePaths.length; i++) {
-        			filePath = filePath + filePaths[i];
-        			if(i != filePaths.length - 1) {
-        				filePath = filePath + "/";
-        			}
-        		}
-        	}
+            String[] filePaths = fileFullPath.split("/");
+            filePath = "/";
+            if(filePaths.length > 2) {
+                for(int i = 2 ; i < filePaths.length; i++) {
+                    filePath = filePath + filePaths[i];
+                    if(i != filePaths.length - 1) {
+                        filePath = filePath + "/";
+                    }
+                }
+            }
         }
         return filePath;
-        
-	}
 
-	@Extension
-	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+    }
+
+    @Extension
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
     public static final class DescriptorImpl extends Descriptor<RepositoryBrowser<?>> {
         public DescriptorImpl() {
