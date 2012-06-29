beb3f97442da599d4affa51fd23142dd81482073
shida <shida@lac.co.jp>
2012-06-29 16:05:23 +0900

    all #ddd forms link to redmine ticket.

--- src/test/java/hudson/plugins/redmine/RedmineLinkAnnotatorTest.java
+++ src/test/java/hudson/plugins/redmine/RedmineLinkAnnotatorTest.java
@@ -10,58 +10,56 @@ public class RedmineLinkAnnotatorTest extends TestCase {
 
     public void testWikiLinkSyntax() {
         assertAnnotatedTextEquals("Nothing here.", "Nothing here.");
+    }
+    public void testWikiLinkSyntax1() {
         assertAnnotatedTextEquals("Text with WikiLink.", "Text with <a href='" + REDMINE_URL + "wiki/WikiLink'>WikiLink</a>.");
+    }
+    public void testWikiLinkSyntax2() {
         assertAnnotatedTextEquals("#42", "<a href='" + REDMINE_URL + "issues/show/42'>#42</a>");
-        assertAnnotatedTextEquals("IssueID 22", "<a href='" + REDMINE_URL + "issues/show/22'>IssueID 22</a>");
-        assertAnnotatedTextEquals("fixes 10,11,12",
-                                  "<a href='" + REDMINE_URL + "issues/show/10'>fixes 10</a>," +
-                                  "<a href='" + REDMINE_URL + "issues/show/11'>11</a>," +
-                                  "<a href='" + REDMINE_URL + "issues/show/12'>12</a>");
-        assertAnnotatedTextEquals("references 110,111,112,113",
-                                  "<a href='" + REDMINE_URL + "issues/show/110'>references 110</a>," +
-                                  "<a href='" + REDMINE_URL + "issues/show/111'>111</a>," +
-                                  "<a href='" + REDMINE_URL + "issues/show/112'>112</a>," +
-                                  "<a href='" + REDMINE_URL + "issues/show/113'>113</a>");
-        assertAnnotatedTextEquals("closes 210, 211",
-                                  "<a href='" + REDMINE_URL + "issues/show/210'>closes 210</a>, " +
-                                  "<a href='" + REDMINE_URL + "issues/show/211'>211</a>");
-        assertAnnotatedTextEquals("closes 210 211",
-                                  "<a href='" + REDMINE_URL + "issues/show/210'>closes 210</a> " +
-                                  "<a href='" + REDMINE_URL + "issues/show/211'>211</a>");
-        assertAnnotatedTextEquals("refs 310, 11, 4, 4120",
-                                  "<a href='" + REDMINE_URL + "issues/show/310'>refs 310</a>, " +
-                                  "<a href='" + REDMINE_URL + "issues/show/11'>11</a>, " +
-                                  "<a href='" + REDMINE_URL + "issues/show/4'>4</a>, " +
-                                  "<a href='" + REDMINE_URL + "issues/show/4120'>4120</a>");
-        assertAnnotatedTextEquals("refs 1&amp;11&amp;111&amp;1111",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>refs 1</a>&amp;" +
-                                  "<a href='" + REDMINE_URL + "issues/show/11'>11</a>&amp;" +
-                                  "<a href='" + REDMINE_URL + "issues/show/111'>111</a>&amp;" +
-                                  "<a href='" + REDMINE_URL + "issues/show/1111'>1111</a>");
-        assertAnnotatedTextEquals("IssueID 21&amp;11&amp;100",
-                                  "<a href='" + REDMINE_URL + "issues/show/21'>IssueID 21</a>&amp;" +
-                                  "<a href='" + REDMINE_URL + "issues/show/11'>11</a>&amp;" +
-                                  "<a href='" + REDMINE_URL + "issues/show/100'>100</a>");
+    }
+    public void testWikiLinkSyntax11() {
         assertAnnotatedTextEquals("refs #1,#11,#111,#1111",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>refs #1</a>," +
+                                  "refs <a href='" + REDMINE_URL + "issues/show/1'>#1</a>," +
                                     "<a href='" + REDMINE_URL + "issues/show/11'>#11</a>," +
                                   "<a href='" + REDMINE_URL + "issues/show/111'>#111</a>," +
                                   "<a href='" + REDMINE_URL + "issues/show/1111'>#1111</a>");
+    }
+    public void testWikiLinkSyntax12() {
         assertAnnotatedTextEquals("refs #1, #11, #111,#1111",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>refs #1</a>, " +
+                                  "refs <a href='" + REDMINE_URL + "issues/show/1'>#1</a>, " +
                                   "<a href='" + REDMINE_URL + "issues/show/11'>#11</a>, " +
                                   "<a href='" + REDMINE_URL + "issues/show/111'>#111</a>," +
                                   "<a href='" + REDMINE_URL + "issues/show/1111'>#1111</a>");
+    }
+    public void testWikiLinkSyntax13() {
         assertAnnotatedTextEquals("refs #1",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>refs #1</a>");
+                                  "refs <a href='" + REDMINE_URL + "issues/show/1'>#1</a>");
+    }
+    public void testWikiLinkSyntax14() {
         assertAnnotatedTextEquals("closes #1&amp;#11",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>closes #1</a>&amp;" +
+                                  "closes <a href='" + REDMINE_URL + "issues/show/1'>#1</a>&amp;" +
                                   "<a href='" + REDMINE_URL + "issues/show/11'>#11</a>");
+    }
+    public void testWikiLinkSyntax15() {
         assertAnnotatedTextEquals("closes #1",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>closes #1</a>");
+                                  "closes <a href='" + REDMINE_URL + "issues/show/1'>#1</a>");
+    }
+    public void testWikiLinkSyntax16() {
         assertAnnotatedTextEquals("IssueID #1 #11",
-                                  "<a href='" + REDMINE_URL + "issues/show/1'>IssueID #1</a> " +
+                                  "IssueID <a href='" + REDMINE_URL + "issues/show/1'>#1</a> " +
                                   "<a href='" + REDMINE_URL + "issues/show/11'>#11</a>");
+    }
+    public void testWikiLinkSyntax17() {
+        assertAnnotatedTextEquals("IssueID #1 #11 sum problem.",
+                                  "IssueID <a href='" + REDMINE_URL + "issues/show/1'>#1</a> " +
+                                  "<a href='" + REDMINE_URL + "issues/show/11'>#11</a> sum problem.");
+    }
+    public void testWikiLinkSyntax18() {
+        assertAnnotatedTextEquals("/abc/be#123", "/abc/be#123");
+    }
+    public void testWikiLinkSyntax19() {
+        // unfortunately, this syntax not suported.
+        //assertAnnotatedTextEquals("/abc/#123", "/abc/#123");
     }
 
     private void assertAnnotatedTextEquals(String originalText, String expectedAnnotatedText) {
@@ -69,8 +67,6 @@ public class RedmineLinkAnnotatorTest extends TestCase {
         for (RedmineLinkAnnotator.LinkMarkup markup : RedmineLinkAnnotator.MARKUPS) {
             markup.process(markupText, REDMINE_URL);
         }
-
-        System.out.println(markupText.toString());
         assertEquals(expectedAnnotatedText, markupText.toString());
     }
 }
